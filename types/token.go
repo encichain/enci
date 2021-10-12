7a35bed0610852fffbcd1ac30388093748874e08
linjiayo <jiagains@gmail.com>
2021-10-12 03:27:49 -0400

    Add cmd query functions

--- types/token.go
+++ types/token.go
@@ -5,21 +5,21 @@ import (
 )
 
 const (
-	MicroTokenDenom = "utoken"
-	MilliTokenDenom = "mtoken"
-	TokenDenom      = "token"
+	MicroTokenDenom = "uenci"
+	MilliTokenDenom = "menci"
+	TokenDenom      = "enci"
 )
 
 var TokenMetaData = banktypes.Metadata{
 	Description: "The native token of EnciChain",
 	DenomUnits: []*banktypes.DenomUnit{
-		{Denom: MicroTokenDenom, Exponent: uint32(0), Aliases: []string{"microtoken"}},
-		{Denom: MilliTokenDenom, Exponent: uint32(3), Aliases: []string{"millitoken"}},
+		{Denom: MicroTokenDenom, Exponent: uint32(0), Aliases: []string{"microenci"}},
+		{Denom: MilliTokenDenom, Exponent: uint32(3), Aliases: []string{"millienci"}},
 		{Denom: TokenDenom, Exponent: uint32(6), Aliases: []string{}},
 	},
 
 	Base:    MicroTokenDenom,
 	Display: TokenDenom,
-	//Name:    "TOKE",
-	//Symbol:  "TOKE",
+	Name:    "ENCI",
+	Symbol:  "ENCI",
 }
--- x/charity/client/cli/query.go
+++ x/charity/client/cli/query.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"fmt"
 
-	//"strings"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -32,8 +33,12 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	cmd.AddCommand(
 		CmdQueryTaxRate(),
 		CmdQueryParams(),
-		CmdQueryCharityOne(),
-		CmdQueryCharityTwo(),
+		CmdQueryCharities(),
+		CmdQueryTaxCap(),
+		CmdQueryTaxCaps(),
+		CmdQueryTaxRateLimits(),
+		CmdQueryTaxProceeds(),
+		CmdQueryCollectionPeriods(),
 	)
 
 	return cmd
@@ -91,11 +96,11 @@ func CmdQueryParams() *cobra.Command {
 }
 
 // CmdQueryCharityOne implements the query Charity One command and returns the charity one object
-func CmdQueryCharityOne() *cobra.Command {
+func CmdQueryCharities() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "charity-one",
+		Use:   "charities",
 		Args:  cobra.NoArgs,
-		Short: "Query charity one",
+		Short: "Query all charities",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
@@ -103,7 +108,7 @@ func CmdQueryCharityOne() *cobra.Command {
 			}
 			queryClient := types.NewQueryClient(clientCtx)
 
-			res, err := queryClient.CharityOne(context.Background(), &types.QueryCharityOneRequest{})
+			res, err := queryClient.Charities(context.Background(), &types.QueryCharitiesRequest{})
 			if err != nil {
 				return err
 			}
@@ -115,12 +120,13 @@ func CmdQueryCharityOne() *cobra.Command {
 	return cmd
 }
 
-// CmdQueryCharityTwo implements the query Charity Two command and returns the charity two object
-func CmdQueryCharityTwo() *cobra.Command {
+// CmdQueryTaxCap implements the query taxcap command and returns the taxcap of a *denom*
+func CmdQueryTaxCap() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "charity-two",
-		Args:  cobra.NoArgs,
-		Short: "Query charity two",
+		Use:   "taxcap [denom]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Query the tax cap of a denom",
+		Long:  "Query the tax cap of a denom. A TaxCap is the maximum amount of tax that can be charged per transaction.",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
@@ -128,7 +134,152 @@ func CmdQueryCharityTwo() *cobra.Command {
 			}
 			queryClient := types.NewQueryClient(clientCtx)
 
-			res, err := queryClient.CharityTwo(context.Background(), &types.QueryCharityTwoRequest{})
+			denom := args[0]
+			res, err := queryClient.TaxCap(context.Background(), &types.QueryTaxCapRequest{Denom: denom})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// CmdQueryTaxCaps implements the query taxcaps command and returns all taxcaps for all denoms
+func CmdQueryTaxCaps() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "taxcaps",
+		Args:  cobra.NoArgs,
+		Short: "Query all existing Taxcaps for all denom assets.",
+		Long:  "Query all existing Taxcaps for all denom assets.. A TaxCap is the maximum amount of tax that can be charged per transaction.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.TaxCaps(context.Background(), &types.QueryTaxCapsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// CmdQueryTaxRateLimits implements the query TaxRateLimits command and returns the limits to TaxRate
+func CmdQueryTaxRateLimits() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "taxratelimits",
+		Args:  cobra.NoArgs,
+		Short: "Query Tax Rate Limits",
+		Long:  "Query Tax Rate Limits. Tax Rate Limits determines the minimum and maximum tax rate that can be set.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.TaxRateLimits(context.Background(), &types.QueryTaxRateLimitsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// CmdQueryTaxProceeds implements the query taxproceeds command and returns the tax collected for the current CollectionPeriod
+func CmdQueryTaxProceeds() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "taxproceeds",
+		Args:  cobra.NoArgs,
+		Short: "Query the tax collected for the current collection period",
+		Long:  "Query the tax collected for the current collection period. This represents the proceeds from the charity tax.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.TaxProceeds(context.Background(), &types.QueryTaxProceedsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// CmdQueryCollectionPeriods implements the query collectionperiods command and returns all CollectionPeriods
+func CmdQueryCollectionPeriods() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "collection-periods",
+		Args:  cobra.NoArgs,
+		Short: "Query all collection periods",
+		Long: strings.TrimSpace(`
+		Query all collection periods.
+		This returns the data from all previous collection periods.
+	
+		$ encid query charity collection-periods
+		`),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.TaxProceeds(context.Background(), &types.QueryTaxProceedsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// CmdQueryCollectionPeriods implements the query collectionperiod command and returns a CollectionPeriod based on period
+func CmdQueryCollectionPeriod() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "collection-period [period]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Query a collection period based on period",
+		Long: strings.TrimSpace(`
+		Query a collection period.
+	
+		$ encid query charity collection-period 0
+		`),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			period, err := strconv.Atoi(args[0])
+			if err != nil {
+				return err
+			}
+
+			res, err := queryClient.CollectionPeriod(context.Background(), &types.QueryCollectionPeriodRequest{Period: uint64(period)})
 			if err != nil {
 				return err
 			}
