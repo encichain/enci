894d356fe7cc223930fefb7c54a12265f311132c
linjiayo <jiagains@gmail.com>
2021-10-16 02:14:26 -0400

    Add test cases for keeper

--- x/charity/keeper/keeper_test.go
+++ x/charity/keeper/keeper_test.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"testing"
+	//"math/rand"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
@@ -45,7 +46,6 @@ func setupKeeper(t testing.TB) (*Keeper, sdk.Context) {
 func TestTaxRateLimits(t *testing.T) {
 	app := CreateTestApp(t)
 
-	// See that we can get and set tax rate
 	for i := int64(0); i < 10; i++ {
 		app.CharityKeeper.SetTaxRateLimits(app.Ctx, types.TaxRateLimits{
 			RateMin: sdk.NewDecWithPrec(i, 3),
@@ -74,6 +74,7 @@ func TestIterateTaxCap(t *testing.T) {
 
 	uenciCap := sdk.NewInt(1000000)
 	input.CharityKeeper.SetTaxCap(input.Ctx, coretypes.MicroTokenDenom, uenciCap)
+	require.Equal(t, input.CharityKeeper.GetTaxCap(input.Ctx, coretypes.MicroTokenDenom), uenciCap)
 
 	input.CharityKeeper.IterateTaxCaps(input.Ctx, func(denom string, taxCap sdk.Int) bool {
 		if denom == coretypes.MicroTokenDenom {
@@ -99,12 +100,12 @@ func TestTaxProceeds(t *testing.T) {
 
 	for i := int64(0); i < 10; i++ {
 		proceeds := sdk.NewCoins(sdk.NewCoin(coretypes.MicroTokenDenom, sdk.NewInt(100+i)))
-		input.CharityKeeper.AddTaxProceeds(input.Ctx, proceeds)
-		input.CharityKeeper.AddTaxProceeds(input.Ctx, proceeds)
-		input.CharityKeeper.AddTaxProceeds(input.Ctx, proceeds)
+		for j := 0; j < 3; j++ {
+			input.CharityKeeper.AddTaxProceeds(input.Ctx, proceeds)
+		}
 
 		require.Equal(t, proceeds.Add(proceeds...).Add(proceeds...), input.CharityKeeper.GetTaxProceeds(input.Ctx))
-
+		require.False(t, input.CharityKeeper.GetTaxProceeds(input.Ctx).IsZero())
 		input.CharityKeeper.SetTaxProceeds(input.Ctx, sdk.Coins{})
 		require.True(t, input.CharityKeeper.GetTaxProceeds(input.Ctx).IsZero())
 	}
@@ -117,4 +118,58 @@ func TestTaxProceeds(t *testing.T) {
 	input.CharityKeeper.SetTaxProceeds(input.Ctx, proceeds)
 	input.CharityKeeper.AddTaxProceeds(input.Ctx, proceeds)
 	require.Equal(t, proceeds.Add(proceeds...), input.CharityKeeper.GetTaxProceeds(input.Ctx))
+	require.False(t, input.CharityKeeper.GetTaxProceeds(input.Ctx).IsZero())
+}
+
+func TestPeriodTaxProceeds(t *testing.T) {
+	testApp := CreateTestApp(t)
+
+	for i := int64(0); i < 10; i++ {
+		// Set TaxProceed to store
+		testApp.CharityKeeper.SetPeriodTaxProceeds(testApp.Ctx, i, sdk.Coins{
+			{Denom: coretypes.MicroTokenDenom, Amount: sdk.NewInt(i*20000 + 100)},
+		})
+
+		// Try to get unset TaxProceed, should return sdk.Coins{}
+		require.Equal(t, sdk.Coins{}, testApp.CharityKeeper.GetPeriodTaxProceeds(testApp.Ctx, i+1))
+
+		require.NoError(t, testApp.CharityKeeper.GetPeriodTaxProceeds(testApp.Ctx, i).Validate())
+		// Check if Get method retrieves valid set TaxProceed
+		require.Equal(t, sdk.Coins{
+			{Denom: coretypes.MicroTokenDenom, Amount: sdk.NewInt(i*20000 + 100)},
+		}, testApp.CharityKeeper.GetPeriodTaxProceeds(testApp.Ctx, i))
+
+		require.NotEqual(t, sdk.Coins{}, testApp.CharityKeeper.GetPeriodTaxProceeds(testApp.Ctx, i))
+		require.False(t, testApp.CharityKeeper.GetPeriodTaxProceeds(testApp.Ctx, i).IsZero())
+	}
+	// Try clearing Tax Proceeds from store
+	testApp.CharityKeeper.ClearPeriodTaxProceeds(testApp.Ctx)
+
+	for i := int64(0); i < 10; i++ {
+		require.Equal(t, sdk.Coins{}, testApp.CharityKeeper.GetPeriodTaxProceeds(testApp.Ctx, i))
+	}
+
+}
+
+func TestPayouts(t *testing.T) {
+	app := CreateTestApp(t)
+
+	for i := int64(0); i < 10; i++ {
+		payouts := []types.Payout{
+			{Denom: coretypes.MicroTokenDenom, Amount: sdk.NewInt(i*sdk.DefaultPowerReduction.Int64() + 1), Recipientaddr: "enci1ftxapr6ecnrmxukp8236wy8sewnn2q530spjn6test"},
+			{Denom: coretypes.MicroTokenDenom, Amount: sdk.NewInt(i*sdk.DefaultPowerReduction.Int64() + 1000), Recipientaddr: "enci1ftxapr6ecnrmxukp8236wy8sewnn2q530spjn6test2"},
+		}
+
+		app.CharityKeeper.SetPayouts(app.Ctx, i, payouts)
+		require.Equal(t, payouts, app.CharityKeeper.GetPayouts(app.Ctx, i))
+	}
+	require.NotEqual(t, []types.Payout{}, app.CharityKeeper.GetPayouts(app.Ctx, 1))
+
+	// Try clearing []Payout from store
+	app.CharityKeeper.ClearPayouts(app.Ctx)
+
+	for i := int64(0); i < 10; i++ {
+		require.Equal(t, []types.Payout{}, app.CharityKeeper.GetPayouts(app.Ctx, i))
+	}
+
 }
