0bdd406f420366b2827ce077785962fcce204ff9
Jiayong Lin <jiagains@gmail.com>
2022-03-03 00:08:05 -0500

    Add x/oracle delegations keeper functions

--- x/oracle/keeper/keeper.go
+++ x/oracle/keeper/keeper.go
@@ -8,6 +8,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/encichain/enci/x/oracle/types"
 )
 
@@ -18,12 +20,12 @@ type (
 		storeKey      sdk.StoreKey
 		memKey        sdk.StoreKey
 		StakingKeeper types.StakingKeeper
-		paramStore    types.ParamSubspace
+		paramStore    paramstypes.Subspace
 	}
 )
 
 // NewKeeper instatiates the oracle keeper
-func NewKeeper(cdc codec.Codec, storeKey, memKey sdk.StoreKey, stakingKeeper types.StakingKeeper, paramStore types.ParamSubspace) *Keeper {
+func NewKeeper(cdc codec.Codec, storeKey, memKey sdk.StoreKey, stakingKeeper types.StakingKeeper, paramStore paramstypes.Subspace) *Keeper {
 
 	// set KeyTable if it has not already been set
 	if !paramStore.HasKeyTable() {
@@ -265,9 +267,9 @@ func (k Keeper) GetAllPrevotes(ctx sdk.Context) []types.Prevote {
 	return prevotes
 }
 
-// GetVoteDelegate returns a delegate address if it exists, given a *Validator* operator address
+// GetVoterDelegate returns a delegate address if it exists, given a *Validator* operator address
 // Return error if no delegate mapped
-func (k Keeper) GetVoteDelegate(ctx sdk.Context, val sdk.ValAddress) (sdk.AccAddress, error) {
+func (k Keeper) GetVoterDelegate(ctx sdk.Context, val sdk.ValAddress) (sdk.AccAddress, error) {
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get(types.GetValDelKey(val))
 
@@ -278,9 +280,9 @@ func (k Keeper) GetVoteDelegate(ctx sdk.Context, val sdk.ValAddress) (sdk.AccAdd
 	return sdk.AccAddress(bz), nil
 }
 
-// GetVoteDelegator returns a validator address if it exists, given a *delegate* address.
+// GetVoterDelegator returns a validator address if it exists, given a *delegate* address.
 // Return error if no validator delegator address mapped
-func (k Keeper) GetVoteDelegator(ctx sdk.Context, del sdk.AccAddress) (sdk.AccAddress, error) {
+func (k Keeper) GetVoterDelegator(ctx sdk.Context, del sdk.AccAddress) (sdk.AccAddress, error) {
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get(types.GetDelValKey(del))
 
@@ -291,9 +293,58 @@ func (k Keeper) GetVoteDelegator(ctx sdk.Context, del sdk.AccAddress) (sdk.AccAd
 	return sdk.AccAddress(bz), nil
 }
 
-// SetVoterDelegation stores a vote delegation in both directions to the store for easy lookup
-func (k Keeper) SetVoteDelegation(ctx sdk.Context, del sdk.AccAddress, val sdk.ValAddress) {
+// SetVoterDelegation stores a voter delegation in both directions to the store for easy lookup
+func (k Keeper) SetVoterDelegation(ctx sdk.Context, del sdk.AccAddress, val sdk.ValAddress) {
 	store := ctx.KVStore(k.storeKey)
 	store.Set(types.GetDelValKey(del), val.Bytes())
 	store.Set(types.GetValDelKey(val), del.Bytes())
+}
+
+// IterateVoterDelegations iterates through all stored voter delegations and performs callback function
+// Stops iteration when no more valid values. Uses only *Validator* address key to reduce redundancy
+func (k Keeper) IterateVoterDelegations(ctx sdk.Context, cb func(val sdk.ValAddress, del sdk.AccAddress) (stop bool)) {
+	store := ctx.KVStore(k.storeKey)
+	iterator := sdk.KVStorePrefixIterator(store, types.ValDelKey)
+
+	for ; iterator.Valid(); iterator.Next() {
+		validator := sdk.ValAddress(iterator.Key()[2:])
+		delegate := sdk.AccAddress(iterator.Value())
+
+		if cb(validator, delegate) {
+			break
+		}
+	}
+}
+
+// GetAllVoterDelegations returns a slice of all stored VoterDelegation
+func (k Keeper) GetAllVoterDelegations(ctx sdk.Context) []types.VoterDelegation {
+	var delegations []types.VoterDelegation
+	k.IterateVoterDelegations(ctx, func(val sdk.ValAddress, del sdk.AccAddress) bool {
+		delegation := types.VoterDelegation{
+			DelegateAddress:  del.String(),
+			ValidatorAddress: val.String(),
+		}
+		delegations = append(delegations, delegation)
+
+		return false
+	})
+	return delegations
+}
+
+// ClearInactiveDelegations deletes all Voter delegations with Validator Unbonded or unknown status from the store
+func (k Keeper) ClearInactiveDelegations(ctx sdk.Context) {
+	store := ctx.KVStore(k.storeKey)
+	k.IterateVoterDelegations(ctx, func(val sdk.ValAddress, del sdk.AccAddress) bool {
+		validator, found := k.StakingKeeper.GetValidator(ctx, val)
+		if !found {
+			store.Delete(types.GetValDelKey(val))
+			store.Delete(types.GetDelValKey(del))
+		}
+
+		if validator.Status < stakingtypes.BondStatus(2) {
+			store.Delete(types.GetValDelKey(val))
+			store.Delete(types.GetDelValKey(del))
+		}
+		return false
+	})
 }
--- x/oracle/types/expected_keepers.go
+++ x/oracle/types/expected_keepers.go
@@ -2,26 +2,11 @@ package types
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
-type (
-	// StakingKeeper defines the staking module interface contract needed by the
-	// claim module.
-	StakingKeeper interface {
-		// ValidatorByConsAddr(sdk.Context, sdk.ConsAddress) stakingexported.ValidatorI
-		Validator(ctx sdk.Context, address sdk.ValAddress) stakingtypes.ValidatorI // get validator by operator address; nil when validator not found
-
-		TotalBondedTokens(sdk.Context) sdk.Int // total bonded tokens within the validator set
-	}
-
-	// ParamSubspace defines the expected Subspace interfacace
-	ParamSubspace interface {
-		HasKeyTable() bool
-		WithKeyTable(table paramtypes.KeyTable) paramtypes.Subspace
-		Get(ctx sdk.Context, key []byte, ptr interface{})
-		GetParamSet(ctx sdk.Context, ps paramtypes.ParamSet)
-		SetParamSet(ctx sdk.Context, ps paramtypes.ParamSet)
-	}
-)
+// StakingKeeper defines the interface for the expected staking keeper
+type StakingKeeper interface {
+	GetValidator(ctx sdk.Context, addr sdk.ValAddress) (validator stakingtypes.Validator, found bool) // get validator by operator address; nil when validator not found
+	TotalBondedTokens(sdk.Context) sdk.Int                                                            // total bonded tokens within the validator set
+}
